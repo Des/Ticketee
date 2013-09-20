acb597aef5fd62e5fbdff58d4f2b7181b4b3ca8f
Des <des.reilly@gmail.com>
2013-09-20 14:06:10 +0100

    Redirect the users back to the projects index page if project they are looking for does not exist.

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,11 @@
 require 'spec_helper'
 
 describe ProjectsController do
+  it "displays an error for a non existent project" do
+    get :show, id: "not there"
+    expect(response).to redirect_to(projects_path)
 
+    message = "The project you were looking for could not be found."
+    expect(flash[:alert]).to eql(message)
+  end
 end
